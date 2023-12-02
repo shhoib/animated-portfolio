9b05e7f75ed0e7479c232992f7574f1ec3163100
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-12-02 19:45:53 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import './app.scss'
+import Navbar from './components/navbar/navbar';
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+     <Navbar/>  
+    </section>
+    <section>parallex</section>
+    <section>services</section>
+    <section>parallex</section>
+    <section>portfolio1</section>
+    <section>portfolio2</section>
+    <section>portfolio3</section>
+    <section>contact</section>
+  </div>;
 };
 
 export default App;
---
+++ src/components/sidebar/togglebutton/toggleButton.jsx
@@ -0,0 +1,8 @@
+
+const ToggleButton = ({setOpen}) => {
+  return (
+    <button onClick={()=>setOpen((prev)=> !prev)}>ToggleButton</button>
+  )
+}
+
+export default ToggleButton
